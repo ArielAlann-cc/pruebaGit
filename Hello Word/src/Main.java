1a5f68493a4d6055a3b060a8144e6cd305c55fce
ArielAlann-cc <alanlive20@gmail.com>
2023-07-29 18:25:37 -0300

    probandoPat

---
+++ Hello Word/src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola como estan gente de youtube");
+        for (int i=0;i<10;i++){
+            System.out.println("es el numero : "+i);
+        }
+
+        System.out.println("----");
+        int i=10;
+
+        while(i>=0){
+            System.out.println("numero n°: "+i);
+            i=i-1;
+        }
+    }
+}
